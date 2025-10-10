5439a1c4517375e65a95dbb79b4c7d14b9de73da
Hayoung Kang <rkdgkdudsam@naver.com>
2025-10-11 02:25:20 +0900

    feat: Home 페이지 퍼블리싱

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,7 +1,46 @@
+import Header from "@/components/home/Header";
+import Gnb from "@/components/common/Gnb";
+import Card from "@/components/common/Card";
+import Filter from "@/components/common/Filter";
+import FloatingButton from "@/components/common/FloatingButton";
+import TopButton from "@/components/common/TopButton";
+import SortSheet from "@/components/common/SortSheet";
+import { useState } from "react";
+import { useModalStore } from "@/stores/useModal";
+import FilterSheet from "@/components/common/FilterSheet";
+
 export default function Home() {
+    const openName = useModalStore((state) => state.openName);
+    const [category, setCategory] = useState("전체");
+    const [sort, setSort] = useState("최신순");
+    const [region, setRegion] = useState<string[]>([]);
+
     return (
         <>
-            <h1>Home Component</h1>
+            <Header setCategory={setCategory} category={category} />
+            <div className="relative w-full min-h-[calc(100%-169px)] bg-gray100">
+                <Filter sort={sort} region={region} />
+                <ul className="p-5 pt-0 flex flex-col gap-2.5 pb-[82px]">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </ul>
+            </div>
+            <FloatingButton filter={region} />
+            <TopButton />
+            <Gnb />
+
+            {openName === "sort" && <SortSheet sort={sort} setSort={setSort} />}
+            {openName === "filter" && (
+                <FilterSheet
+                    sort={sort}
+                    region={region}
+                    setSort={setSort}
+                    setRegion={setRegion}
+                />
+            )}
         </>
     );
 }
