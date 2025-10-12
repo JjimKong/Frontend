ba715810e10a6b7f8941d0ea7048c1ad124b58ad
Hayoung Kang <rkdgkdudsam@naver.com>
2025-10-12 13:39:10 +0900

    feat: Button 컴포넌트 아이콘 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,24 @@
+import { twMerge } from "tailwind-merge";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     color?: "main" | "gray";
+    icon?: string;
+    className?: string;
 }
 
 export default function Button({
     color,
     children,
     disabled,
+    icon,
+    className,
     ...props
 }: ButtonProps) {
     return (
         <>
             <button
-                className={`w-full h-[50px] rounded-xl duration-200 ease-in-out
+                className={twMerge(
+                    `relative w-full h-[50px] rounded-xl duration-200 ease-in-out
                 ${
                     disabled
                         ? "bg-gray200 dark:bg-gray900 text-gray500 dark:text-gray600 cursor-default"
@@ -20,9 +27,18 @@ export default function Button({
                         : color === "gray"
                         ? "bg-gray200 text-gray1000 active:bg-gray300"
                         : "bg-gray1000 text-white active:bg-gray900"
-                }`}
+                }`,
+                    className
+                )}
                 {...props}
             >
+                <div className="absolute pl-5 flex justify-center gap-2">
+                    {icon && (
+                        <span className="relative aspect-[1/1] w-[20px]">
+                            <img src={icon} alt="아이콘" />
+                        </span>
+                    )}
+                </div>
                 {children}
             </button>
         </>
