0c6fcba0ac8f8ef70652a0835e37688beee59a8e
Hayoung Kang <rkdgkdudsam@naver.com>
2026-04-10 13:10:02 +0900

    refactor: Input 컴포넌트 안쓰는 props 삭제

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,15 +3,11 @@ import { twMerge } from "tailwind-merge";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string;
-    error?: boolean;
-    errorMsg?: string;
     icon?: React.ReactNode;
     children?: React.ReactNode;
 }
 
 export default function Input({
-    error,
-    errorMsg,
     className = "",
     icon,
     children,
@@ -24,21 +20,11 @@ export default function Input({
                 <input
                     value={value}
                     className={twMerge(
-                        `peer text-gray1000 placeholder-gray500 h-12 w-full border-b border-gray-300 duration-200 ease-in-out focus:outline-none ${error && "border-red-600"} ${icon && "pr-10"}`,
+                        `peer text-gray1000 placeholder-gray500 h-12 w-full border-b border-gray-300 duration-200 ease-in-out focus:outline-none`,
                         className,
                     )}
                     {...props}
                 />
-
-                <label
-                    className={`h6 pl-2 text-red-600 transition-all duration-200 ease-in-out ${
-                        error
-                            ? "max-h-[40px] translate-y-0 opacity-100"
-                            : "max-h-0 -translate-y-2 opacity-0"
-                    }`}
-                >
-                    {errorMsg}
-                </label>
 
                 <label
                     className={`text-gray500 absolute left-0 -translate-y-1/2 duration-200 peer-focus:top-0 peer-focus:text-xs ${value ? "top-0 text-xs" : "top-1/2"}`}
